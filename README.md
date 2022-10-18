498a00f7080d53e5d46e35b991c546c21b2c4e8b
wrmacdonald <wrmacdonald2016@gmail.com>
2022-10-18 12:40:19 -0700

    - add documentation & clean up

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # cl-server-proj
 
+## Running project locally with Docker:
 
-#### start DB with Docker:
+#### 1st: start postgres DB containers:
 
 `docker compose up -d db1`
 
-#### access DB:
-`docker exec -it db1 psql -U postgres`
+`docker compose up -d db_test`
 
-#### start Flask server with Docker:
+
+#### 2nd: start Flask server:
+
+(will also create necessary tables in database)
+
 `docker compose up --build flask-app`
 
+
+#### to access DB:
+
+`docker exec -it db1 psql -U postgres`
+
+
+#### to run unit tests:
+
+Go into the Flask server Docker container CLI & run:
+
+`python -m unittest discover -p unit_tests.py`
 
--- unit_tests.py
+++ unit_tests.py
@@ -1,6 +1,4 @@
-from flask import Flask
 from flask_testing import TestCase
-from flask_sqlalchemy import SQLAlchemy
 from app import app, db
 import os
 
@@ -21,22 +19,6 @@ class BaseTestCase(TestCase):
     def test_2(self):
         response = self.client.get('/bad_users')
         self.assertEqual(response.status_code, 404)
-
-    # def test_users_1(self):
-    #     response = self.client.get('/users')
-    #     app.logger.info(response.data)
-    #     self.assertEqual(response.data, [])
-
-    # def test_users_2(self):
-    #     test_data = dict(
-    #         address='123 Test',
-    #         email='unittest.com',
-    #         image='www.unit_test-image.com',
-    #         name='unittest')
-    #     response = self.client.post('/users', data=test_data)
-    #     app.logger.info(response.data)
-    #     self.assertEqual(response.status_code, 200)
-    #     # self.assertEqual(response.data, [])
 
     def tearDown(self):
         db.session.remove()
